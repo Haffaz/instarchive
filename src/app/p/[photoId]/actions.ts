fae25f23751cbca03bfb4b55138b771f92a139d7
Haffaz <haffaz.se@gmail.com>
2024-09-08 12:16:12 +0200

    use key from env

--- src/app/p/[photoId]/actions.ts
+++ src/app/p/[photoId]/actions.ts
@@ -7,16 +7,15 @@ export async function generateImage(image: string, prompt: string) {
 
   formData.append('image', blob);
   formData.append('prompt', prompt);
-  formData.append('control_strength', '0.6');
   formData.append('output_format', 'webp');
 
   const response = await fetch(
-    'https://api.stability.ai/v2beta/stable-image/control/structure',
+    'https://api.stability.ai/v2beta/stable-image/control/style',
     {
       method: 'POST',
       body: formData,
       headers: {
-        Authorization: `Bearer sk-J5b4uKHDrh6fYDllRniAJaz5jxzPVkcwhtM7Kx9uWgtbBAiR`,
+        Authorization: `Bearer ${process.env.STABILITY_AI_KEY}`,
         Accept: 'image/*',
       },
     },
